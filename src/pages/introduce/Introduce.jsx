8cb419d8e1ab41d9d53a565c6d9372163b536293
parkjonghwan22 <gch02933@naver.com>
2023-07-27 15:56:55 +0900

    feat: introduce 반응형추가

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -2,16 +2,22 @@
 export const Introduce = () => {
     return (
         <>
-            <div className="mx-auto bg-[#232a3f] h-[500px]">
-                <div className="w-3/5 h-full mx-auto mt-40">
-                    <div className="flex flex-col text-center pt-4 h-full">
+            <div className="mx-auto bg-[#232a3f] lg:h-[500px]">
+                <div className="w-full lg:w-3/5 h-full mx-auto mt-40">
+                    <div className="flex flex-col text-center h-full">
                         <h1 className="font-bold text-6xl text-white">'박종환'</h1>
-                        <span className="font-bold text-6xl text-white pt-6">웹 개발자 포트폴리오</span>
+                        <div className="pt-6 flex flex-col lg:flex-row lg:mx-auto">
+                        <span className="font-bold text-6xl text-white lg:mr-4">웹 개발자</span>
+                        <span className="font-bold text-6xl text-white pt-4 lg:pt-0">포트폴리오</span>
+                        </div>
                         <div className="w-20 px-8 py-0.5 bg-[#E6A15C] rounded-lg mx-auto mt-12"></div>
-                        <div className="flex flex-col text-xl font-semibold text-[#8f98b3] pt-8">
+                        <div className="flex flex-col text-sm font-semibold lg:text-xl lg:font-semibold text-[#8f98b3] pt-8 w-4/6 mx-auto lg:w-full">
                             <span>안녕하세요</span>
-                            <span className="pt-2">이벤트 동작 하나하나에 즐거움을 느끼는 웹 개발자입니다</span>
-                            <span className="pt-2">즐겁게 개발할 수 있으며, 같이 협업을 하는 것을 좋아하는 것이 장점입니다</span>
+                            <span className="pt-2">이전에 다른 분야에서 사람들에게 가치를 제공하고 서비스를 제작하여 판매한 경험이 있습니다</span>
+                            <span className="pt-2">다양한 환경과 사람들과의 만남을 즐기며, 이를 통해 협업을 원활하게 이끌어 낼 수 있습니다</span>
+                            <span className="pt-2">즐겁게 개발하는 것을 좋아하며, 함께하는 팀원들과의 협업을 통해 더 나은 결과물을 만들어내기를 기대합니다</span>
+                            <span className="pt-2">저의 강점은 환경에 빠르게 적응하고, 유연하게 동작하며, 새로운 도전에 즐거움을 느끼는 것입니다</span>
+                            <span className="pt-2">함께 협력하여 좋은 결과를 만들어내는 일에 열정을 가지고 있으며, 항상 성장하고 발전하는 개발자로 남고 싶습니다</span>
                         </div>
                     </div>
                 </div>
